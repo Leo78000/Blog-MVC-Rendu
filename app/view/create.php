0730f0192b46aa1c8ea2b521648ee9bb9403d785
Léonard MARTIN <leo.martindv@gmail.com>
2020-01-26 13:08:51 +0100

    first commit

---
+++ app/view/create.php
@@ -0,0 +1,30 @@
+
+
+    
+<div class="container-fluid">
+<form action="/mvc2/?page=create" method="post" class="mt-5">
+
+        <div class="form-group">
+            <label for="title">Titre: </label>
+            <input type="text" name="title" class="form-control" id="title" placeholder="Title" required>
+        </div>
+        <div class="form-group">
+            <label for="text">Description: </label>
+            <textarea name="text" id="text" class="form-control" rows="5" placeholder="Description"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="category">Categorie: </label>
+            <input type="text" name="category" id="category" class="form-control" placeholder="Catégorie"></input>
+        </div>
+
+        <div class="form-group">
+            <label for="date">Date: </label>
+            <input type="date" id="date" name="date" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-success" name="create_article" id="create_article">Ajouter l'article !</button>
+        
+    </form>
+
+</div>
---
+++ app/view/show.php
@@ -0,0 +1,10 @@
+
+<div class="container-fluid">
+<h1>SHOW ARTICLE</h1>
+<h2> <?= $article[0]["title"] ?></h2>     
+<p> <?= $article[0]["text"] ?></p>  
+<div>
+    <a href="/mvc2/" class="btn btn-danger">Retour</a>
+</div>
+</div>
+ 
